6cdd84970228d2fa17280e5e07d56243dea505ea
Chris Speck <cgspeck@gmail.com>
2015-07-21 10:08:08 +1000

    Prepare package for 1.0.0 release and publish to Pypi

--- setup.py
+++ setup.py
@@ -4,17 +4,20 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 with open('requirements-test.txt') as f:
-    required_test = f.read().splitlines()
+    required_test = [x for x in f.read().splitlines() if not x.startswith('-r')]
+    required_test.extend(required)
 
 setup(
     name='ENI Arbiter',
     description='''
     AWS Elastic Network Interface Arbiter
     ''',
-    version='0.9.0',
+    version='1.0.0',
     author='Chris Speck',
-    author_email='cgspeck@gmail.com',
-    url='http://www.chrisspeck.com/',
+    author_email='chris@chrisspeck.com',
+    url='https://github.com/cgspeck/eniarbiter',
+    download_url = 'https://github.com/cgspeck/eniarbiter/tarball/1.0.0',
+    keywords = ['AWS', 'ec2', 'ENI', 'boto'],
     packages=['eniarbiter'],
     entry_points={
         'console_scripts': [
