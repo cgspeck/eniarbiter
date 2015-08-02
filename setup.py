27e8f745923cd6e7401b2dd035502c2da85bd77e
Chris Speck <cgspeck@gmail.com>
2015-08-02 17:18:54 +1000

    Update description and keywords

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open('requirements-test.txt') as f:
 setup(
     name='eniarbiter',
     description='''
-    AWS Elastic Network Interface Arbiter/Assigner
+    AWS Elastic Network Interface & Elastic IP Address Assigner
     ''',
-    version='1.3.1',
+    version='1.3.2',
     author='Chris Speck',
     author_email='chris@chrisspeck.com',
     url='https://github.com/cgspeck/eniarbiter',
-    download_url='https://github.com/cgspeck/eniarbiter/tarball/1.3.1',
-    keywords=['AWS', 'ec2', 'ENI', 'boto', 'Elastic IP Address', 'AWS Public IP Address'],
+    download_url='https://github.com/cgspeck/eniarbiter/tarball/1.3.2',
+    keywords=['AWS', 'ec2', 'ENI', 'boto', 'Elastic IP Address', 'Elastic Network Interface'],
     packages=['eniarbiter'],
     entry_points={
         'console_scripts': [
